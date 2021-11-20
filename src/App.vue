2bc5ec951d8175353abb49926a6e121bf2aaa945
Asliddin <hhamrayev60@gmail.com>
2021-11-20 13:16:13 +0500

    a

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
+/* eslint-disable vue/no-unused-components */
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Left />
+    <Right />
   </div>
 </template>
-
+<script>
+import Left from "./components/Left.vue";
+import Right from "./components/Right.vue"
+export default {
+  name: "app",
+  components: {
+    Left,
+    Right,
+  },
+}
+</script>
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  display: flex;
+  max-width: 1400px;
 }
 </style>
---
+++ src/components/Left.vue
@@ -0,0 +1,97 @@
+<template>
+  <main>
+    <div class="left-content">
+      <div class="content">
+        <p class="logo">Hamrayev.uz</p>
+
+        <h2 class="main__title">
+          Be Creative, <br />
+          Be Impressive
+        </h2>
+        <h4 class="title">Creative Designer and Social Media Officer</h4>
+
+        <p class="main__text">
+          I have to be able to communicate ideas to a wide range of stakeholders
+          in both my own voice and in Sprout's. Communicating on social.
+        </p>
+
+        <div class="flex">
+          <button class="btn__portfolio">Portfolio</button>
+          <a href="#" class="link__vitae">Curriculum Vitae</a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap");
+.left-content {
+  background: linear-gradient(146.88deg, #1e1f1d 1.46%, #1a1c18 99.06%);
+  width: 700px;
+  font-family: "Poppins", sans-serif;
+  color: #fff;
+  height: 100vh;
+  padding: 25px;
+}
+
+.content {
+  max-width: 435px;
+  margin: 0 auto;
+}
+
+.logo {
+  font-size: 18px;
+  padding: 5px;
+  border: 2px solid #fff;
+  font-weight: 300;
+  display: inline;
+}
+
+.main__title {
+  font-weight: 600;
+  margin-top: 80px;
+  text-align: left;
+  font-size: 50px;
+  line-height: 55px;
+}
+
+.title {
+  font-weight: 300;
+  margin: 15px 0;
+}
+
+.main__text {
+  font-weight: normal;
+  font-size: 16px;
+  font-weight: 300;
+  font-size: 16px;
+  color: #cfcfcf;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+  margin-top: 35px;
+  justify-content: space-around;
+}
+
+.btn__portfolio {
+  background-color: #fff;
+  border: none;
+  padding: 15px 28px;
+  border-radius: 25px;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+.link__vitae {
+  color: #fff;
+  text-decoration: underline;
+  font-weight: 200;
+}
+</style>
