c111d3fa3de8af78fd5d067522361089202571d4
Asliddin <hhamrayev60@gmail.com>
2021-11-30 15:40:03 +0500

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Login page</h1>
+  <div class="login">
+      <div class="login-box">
+          <h1 class="login__title">Login Page</h1>
+
+          <div class="login-page">
+              
+          </div>
+          <form action="">
+              <img src="../assets/Frame.png" alt="" class="login__img">
+            <h4 class="main__text">Email</h4>
+            <input type="email" class="input email" placeholder="Your email">
+
+            <h4 class="main__text">Password</h4>
+            <input type="password" class="input password" name="" id="" placeholder="Your password">
+            <img src="../assets/view.png" class="i-view" alt="" @click="show">
+
+            <button class="enter">Kirish</button>
+          </form>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +28,79 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap');
+.login {
+    height: 100vh;
+    background-color: #21D4FD;
+    background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+    display: flex;
+    align-items: center;
+    font-family: 'Dongle', sans-serif;
+    color: #fff;
+}
 
+.login-box {
+    width: 100%;
+    text-align: center;
+    max-width: 260px;
+    margin: 0 auto;
+}
+
+.login__title {
+    font-weight: 300;
+}
+
+.main__text {
+    margin-top: 24px;
+    text-align: left;
+    font-size: 14px;
+}
+.input {
+    padding: 10px;
+    width: 100%;
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #333;
+    margin-top: 15px;
+    outline: none;
+    color: #fff;
+}
+
+.input::placeholder {
+    color: #fff;
+}
+
+
+         .enter {background-image: linear-gradient(to right, #314755 0%, #26a0da  51%, #314755  100%)}
+         .enter {
+            padding: 15px 45px;
+            text-align: center;
+            text-transform: uppercase;
+            transition: 0.5s;
+            background-size: 200% auto;
+            color: white;            
+            border-radius: 10px;
+            display: block;
+            border: none;
+            margin: 0 auto;
+            margin-top: 26px;
+          }
+
+          .enter-grad:hover {
+            background-position: right center;
+            color: #fff;
+            text-decoration: none;
+          }
+
+
+.login__img {
+    width: 230px;
+}
+
+.i-view {
+    width: 24px;
+    transform: translate(115px, -32px);
+    opacity: 50%;
+}
+         
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,10 @@
 <template>
 <div class="register">
-    <img src="../assets/background.png" class="bg" alt="">
-    <div class="register-box">
-        <h1>Register</h1>
+        <div class="register-box">
+
+        <p class="register__text">Har kuningiz rejalar bilan boshlang</p>
+
+        <router-link to="login"><button class="register__btn">Kunni boshlash</button></router-link>
     </div>
 </div>
 </template>
@@ -14,13 +16,38 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap');
+.register {
+    height: 100vh;
+    background-color: #21D4FD;
+    background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+    display: flex;
+    align-items: center;
+    font-family: 'Dongle', sans-serif;
+    color: #fff;
+}
 
-#app {
-    background-color: transparent;
+.register-box {
+    text-align: center;
+    padding: 20px;
+    width: 100%;
 }
-.bg {
-    position: fixed;
-    height: auto;
-    z-index: -2;
+
+
+.register__text {
+    text-align: center;
+    font-size: 26px;
+    font-weight: 600;
 }
+
+.register__btn {
+    padding: 15px;
+    background: transparent;
+    border: 2px solid #fff;
+    color: #fff;
+    font-weight: 600;
+    border-radius: 20px;
+    margin-top: 25px;
+}
+
 </style>
